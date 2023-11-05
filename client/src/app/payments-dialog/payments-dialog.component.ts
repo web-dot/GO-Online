6cb458195aca382886eabdb789a1a0278de65046
sudipto <cg.sudipc@gmail.com>
2023-11-05 21:32:09 +0530

    created payment dialog for razorpay integration

--- client/src/app/payments-dialog/payments-dialog.component.ts
+++ client/src/app/payments-dialog/payments-dialog.component.ts
@@ -1,23 +1,30 @@
+// payments-dialog.component.ts
 import { Component } from '@angular/core';
 import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
-  selector: 'app-payment-dialog',
+  selector: 'app-payments-dialog',
   templateUrl: './payments-dialog.component.html',
   styleUrls: ['./payments-dialog.component.css']
 })
 export class PaymentsDialogComponent {
   selectedPaymentMethod: string;
 
-  constructor(public dialogRef: MatDialogRef<PaymentsDialogComponent>) {}
-
-  selectPaymentMethod() {
-    // Close the dialog and pass the selected payment method back to the parent component
-    this.dialogRef.close(this.selectedPaymentMethod);
+  constructor(public dialogRef: MatDialogRef<PaymentsDialogComponent>) {
+    this.selectedPaymentMethod = ''; // Default selection
   }
 
-  cancel() {
-    // Close the dialog without selecting a payment method
-    this.dialogRef.close();
+  payNow() {
+    // Handle payment based on the selected payment method
+    if (this.selectedPaymentMethod === 'razorpay') {
+      // Initialize Razorpay and trigger payment modal
+      // Handle payment response within this block
+      // Close the dialog after payment is complete
+    }
+
+    // You can handle other payment methods in a similar way
+
+    // Close the dialog after payment
+    this.dialogRef.close(/* Payment status or any other data */);
   }
 }
